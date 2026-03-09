d3499f2057e07ef7a3a22999c927cb5ba357035b
Prajwal7387 <prajwalmahamuni@gmail.com>
2026-03-09 21:05:21 +0530

    Fixed Render port configuration

--- app.py
+++ app.py
@@ -69,3 +69,9 @@ def delete_student(student_id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
